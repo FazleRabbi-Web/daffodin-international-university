c2e4328d8a58b038a62c3e8d5f62b9d3d0d0f10c
FazleRabbi-Web <muhammadfazlerabbi86@gmail.com>
2021-10-21 22:50:21 +0600

    solved by siddiq vaia

--- src/conponents/Footer/Footer.js
+++ src/conponents/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='mt-5 pt-5'>
-            <h4 className='bg-warning fw-bold py-3' >Copyright © 2021 Daffodil International University. All Rights Reserved.</h4>
+            <h4 className='bg-warning fw-bold py-3' >Copyright © 2021 Daffodil International University, All Rights Reserved.</h4>
         </div>
     );
 };
